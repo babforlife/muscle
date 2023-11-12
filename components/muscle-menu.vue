11fb9759c5306b3ec59d95aab7fcffa2785e7dc6
Guillaume Malek <guillaume.malek@outlook.fr>
2023-11-12 23:22:44 +0100

    feat: add dashboard

--- components/muscle-menu.vue
+++ components/muscle-menu.vue
@@ -1,7 +1,8 @@
 <template>
   <div :class="{ hidden: !open }" class="comp-menu">
-    <div class="backdrop" @click="open = false"></div>
+    <div class="backdrop" @click="open = false" />
     <div class="fixed flex flex-col p-5 gap-2 h-full border z-20 bg-gray-100 shadow-2xl w-1/2">
+      <NuxtLink to="/" @click="open = false">Dashboard</NuxtLink>
       <NuxtLink to="/sessions" @click="open = false">Séances</NuxtLink>
       <NuxtLink to="/exercises" @click="open = false">Exercices</NuxtLink>
     </div>
@@ -14,12 +15,12 @@ import { on } from 'shuutils'
 export default {
   data() {
     return {
-      open: false,
+      open: false
     }
   },
   beforeMount() {
-    on('toggle-menu', (open: boolean) => this.open = open)
-  },
+    on('toggle-menu', (open: boolean) => (this.open = open))
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,44 @@
 <template>
-  <div></div>
+  <div class="flex gap-2 text-2xl flex-col justify-center items-center h-full">
+    <span>Choisissez une exercice à démarrer</span>
+    <div v-for="session in sessions" :key="session._id" :class="{ active: session._id === active._id }" @click="active = session">
+      {{ session.name }}
+    </div>
+    <NuxtLink v-if="active._id" :to="'/running/' + active._id" class="border-2 p-1 rounded" @click="start">Démarrer</NuxtLink>
+  </div>
 </template>
+
+<script lang="ts">
+import { emit } from 'shuutils'
+import type { Session } from '~/models'
+import { sessionsService } from '~/services'
+
+export default {
+  data() {
+    return {
+      active: {} as Session,
+      sessions: [] as Session[]
+    }
+  },
+  async beforeMount() {
+    emit('header-title', 'Dashboard')
+    this.sessions = await this.getAll()
+  },
+  methods: {
+    async getAll() {
+      return await sessionsService.getAll().catch(() => {
+        throw new Error('Failed to get sessions')
+      })
+    },
+    start() {
+      emit('start-session', this.active)
+    }
+  }
+}
+</script>
+
+<style>
+.active {
+  color: red;
+}
+</style>
