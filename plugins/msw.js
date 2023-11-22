7a8afdb94e3b74aa8f8c85f745b1f3bc35d0502e
Guillaume Malek <guillaume.malek@outlook.fr>
2023-11-22 18:47:39 +0100

    feat: mock session requests

--- plugins/msw.js
+++ plugins/msw.js
@@ -1,16 +1,25 @@
 import { http, HttpResponse } from 'msw'
 import { setupWorker } from 'msw/browser'
-import { sessions, exercises } from '~/mock'
+import { sessions, exercises, pectoraux } from '~/mock'
 import { exercisesService, sessionsService } from '~/services'
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
-  if (process.env.NODE_ENV !== 'development' && !config.public.mock) return
-  return setupWorker(...sessionsHandlers, ...exercisesHandlers).start()
+  if (process.env.NODE_ENV !== 'development' || !config.public.mock) return
+  return setupWorker(...sessionsHandlers, ...exercisesHandlers).start({
+    onUnhandledRequest(request, print) {
+      const excludedRequests = ['/favicon.ico', '/_nuxt/']
+      if (excludedRequests.some(exc => request.url.includes(exc))) return
+      print.warning()
+    }
+  })
 })
 
 const sessionsHandlers = [
-  http.get(sessionsService.sessionsUrl, () => HttpResponse.json([...sessions]))
+  http.get(sessionsService.sessionsUrl, () => HttpResponse.json([...sessions])),
+  http.get(`${sessionsService.sessionsUrl}/1`, () => HttpResponse.json(pectoraux)),
+  http.get(`${sessionsService.sessionsUrl}/2`, () => HttpResponse.json(dos)),
+  http.get(`${sessionsService.sessionsUrl}/*`, () => HttpResponse.json(pectoraux))
 ]
 
 const exercisesHandlers = [
