204bbdd026aa9758c822013b8586c4e68a7a375a
Guillaume Malek <guillaume.malek@outlook.fr>
2023-12-03 14:43:19 +0100

    feat: style programs

--- pages/programs/[id].vue
+++ pages/programs/[id].vue
@@ -1,68 +1,128 @@
 <script setup lang="ts">
-import { emit } from 'shuutils'
+import { ChevronUpDownIcon, TrashIcon } from '@heroicons/vue/24/solid'
+import { emit, on } from 'shuutils'
 import draggable from 'vuedraggable'
 import { Exercise, Program } from '~/models'
 import { exerciseService, programService } from '~/services'
+import { romanize } from '~/utils'
 
 const exercises = ref([] as Exercise[])
 const program = ref(new Program())
+const modalState = ref('' as undefined | 'delete' | 'edit' | 'add')
+const edit = ref(false)
+
+const updateName = ref('')
+const exercisesNotInProgram = ref([] as { _id: string, name: string, checked: boolean }[])
+on('edit', () => modalState.value = 'edit')
 
 onMounted(async () => {
   emit('header', { title: 'Programme', navigation: true, edit: true })
   const route = useRoute()
-  const id = route.params.id as string
-  if (id !== 'create') program.value = await programService.get(id).catch(() => { throw new Error('Failed to get program') })
+  program.value = await programService.get(route.params.id).catch(() => { throw new Error('Failed to get program') })
   exercises.value = await exerciseService.getAll().catch(() => { throw new Error('Failed to get exercises') })
-  emit('header-title', program.value.name)
+  updateName.value = program.value.name
+  setExercisesNotInProgram()
 })
 
-function add(exercise: Exercise) {
-  program.value.exercises.push(exercise)
+const exercisesChecked = computed(() => exercisesNotInProgram.value.filter(exercise => exercise.checked))
+
+function setExercisesNotInProgram() {
+  exercisesNotInProgram.value = exercises.value
+    .filter((exercise: Exercise) => !program.value.exercises.some((ex: Exercise) => ex._id === exercise._id))
+    .map(exercise => ({ ...exercise, checked: false }))
 }
 
-function changeColor() {
-  program.value.color = Math.random()
+async function addExercises() {
+  program.value.exercises.push(...exercisesChecked.value)
+  await programService.save(program.value).catch(error => console.log('catch error,', error))
+  modalState.value = undefined
+  setExercisesNotInProgram()
 }
 
-function color() {
-  return `hsl(${360 * program.value.color}, 80%, 60%)`
+function removeExercise(exercise: Exercise) {
+  program.value.exercises = program.value.exercises.filter(ex => ex._id !== exercise._id)
 }
 
-function save() {
-  programService.save(program.value)
+async function save() {
+  program.value.name = updateName.value
+  await programService.save(program.value).catch(error => console.log('catch error,', error))
+  modalState.value = undefined
 }
 
-function remove(exercise: Exercise) {
-  program.value.exercises = program.value.exercises.filter((ex: Exercise) => ex._id !== exercise._id)
+async function remove() {
+  await programService.delete(program.value).then(async () => await navigateTo('/programs')).catch(() => console.log('Catch error'))
 }
-
-const exercisesNotInProgram = computed(() => {
-  if (program.value.exercises === undefined) return exercises.value
-  return exercises.value.filter((exercise: Exercise) => !program.value.exercises.some((ex: Exercise) => ex._id === exercise._id))
-})
 </script>
 
 <template>
-  <div class="page-program">
-    <div class="flex items-center gap-2">
-      <input
-        v-model="program.name"
-        class="h-10 flex-1 px-3 my-3 placeholder-gray-600 border rounded-lg focus:shadow-outline"
-        placeholder="Nom de la séance"
-      >
-      <div class="w-1/6 h-10 rounded-lg border-2 border-gray-200" :style="{ 'background-color': color() }" @click="changeColor()" />
+  <div class="col overflow-hidden h-full">
+    <h2>{{ program.name }}</h2>
+    <draggable v-model="program.exercises" :disabled="!edit" :item-key="program._id" class="col gap-3 w-full min-w-[80%] flex-1 overflow-auto">
+      <template #item="{element, index}">
+        <div class="row w-full items-center justify-around px-4">
+          <ChevronUpDownIcon v-if="edit" class="icon-8" />
+          <span class="bg-indigo-200 font-bold w-12 h-12 rounded-full relative left-3 flex justify-center items-center">{{ romanize(index+1) }}</span>
+          <span class="row text-gray-700 rounded-r-xl bg-indigo-200 w-full justify-center items-center py-1">
+            {{ element.name }}</span>
+          <TrashIcon v-if="edit" class="icon-8" @click="removeExercise(element)" />
+        </div>
+      </template>
+    </draggable>
+    <div class="p-2 row justify-evenly">
+      <button theme="primary" @click="edit=!edit">{{ edit? 'Annuler' : 'Modifier' }}</button>
+      <button v-if="edit" theme="primary" @click="() => {save(); edit=false}">Valider</button>
+      <button v-if="!edit" theme="primary" @click="modalState = 'add'">Ajouter un exercice</button>
     </div>
-    <div class="flex flex-col">
-      <draggable v-model="program.exercises" :item-key="program._id">
-        <template #item="{element}">
-          <div @click="remove(element)">{{ element.name }}</div>
-        </template>
-      </draggable>
-    </div>
-    <span class="underline">Ajouter des exercices :</span>
-    <div class="flex flex-col">
-      <span v-for="exercise in exercisesNotInProgram" :key="exercise._id" @click="add(exercise)">{{ exercise.name }}</span>
-    </div>
-    <NuxtLink to="/programs"> <button @click="save">Enregistrer</button></NuxtLink>
+    <Modal v-if="modalState" class="px-10 modal" child-class="w-full" :state="modalState" @close="modalState=undefined">
+      <template v-if="modalState === 'edit'">
+        <h2>Modification</h2>
+        <label>Nom
+          <input v-model="updateName" v-focus @keyup.enter="save">
+        </label>
+        <div class="flex gap-3 justify-center">
+          <button theme="primary" @click="save">Enregistrer</button>
+          <button theme="primary" @click="modalState=undefined">Fermer</button>
+        </div>
+        <hr>
+        <h2>Supprimer ce programme</h2>
+        <button class="w-fit m-auto" theme="danger" @click="modalState='delete'">Supprimer</button>
+      </template>
+      <template v-if="modalState === 'add'">
+        <h2>Cliquer sur un exercice pour le sélectionner</h2>
+        <div class="col flex-1 overflow-auto text-lg">
+          <div v-for="exercise in exercisesNotInProgram" :key="exercise._id" class="cursor-pointer" @click="exercise.checked=!exercise.checked">
+            <span class="custom-checkbox" :selected="exercise.checked">
+              {{ exercise.name }}
+            </span>
+          </div>
+        </div>
+        <button :disabled="exercisesChecked.length === 0" theme="primary" @click="addExercises">Ajouter</button>
+        <button theme="primary" @click="modalState=undefined">Fermer</button>
+      </template>
+      <template v-if="modalState === 'delete'">
+        <h2>Suppression</h2>
+        <span class="text-center">Êtes-vous sûr de vouloir supprimer le programme '{{ program.name }}' ?
+        </span>
+        <div class="flex gap-3 justify-center text-xl">
+          <button theme="danger" @click="remove">Oui</button>
+          <button theme="primary" @click="modalState=undefined">Annuler</button>
+        </div>
+      </template>
+    </Modal>
   </div>
 </template>
+
+<style>
+.modal[state="add"]>div {
+  max-height: 75%;
+  overflow: hidden;
+}
+
+.custom-checkbox[selected="true"] {
+  @apply text-indigo-500 font-medium;
+}
+
+.custom-checkbox[selected="false"] {
+  @apply text-gray-400;
+}
+</style>
--- pages/programs/index.vue
+++ pages/programs/index.vue
@@ -1,33 +1,52 @@
 <script setup lang="ts">
+import { PlusCircleIcon } from '@heroicons/vue/24/solid'
 import { emit } from 'shuutils'
 import { programService } from '~/services'
 import { Program } from '~/models'
-import { hueToColor } from '~/utils'
 
 const programs = ref([] as Program[])
+const newProgram = ref(new Program())
+const showModal = ref(false)
 
 onMounted(async () => {
   emit('header', { title: 'Programmes', search: true })
+  await get()
+})
+
+async function get() {
+  await programService.getAll().then(programsUpdate => programs.value = programsUpdate).catch(() => console.log('Failed to get exercises'))
+}
+
+async function save() {
+  await programService.save(newProgram.value).then(() => get()).catch(() => console.log('Failed to save exercise'))
+  showModal.value = false
+}
 </script>
 
 <template>
-  <div class="page-programs h-full flex flex-col gap-4 mt-4">
-    <NuxtLink
-      v-for="(program, index) in programs"
-      :key="program._id"
-      :to="'/programs/' + program._id"
-      class="flex flex-col justify-between items-center p-4 mx-4 border-2 rounded-xl border-gray-300"
-      :class="{ 'mb-20': index === programs.length - 1 }"
-      :style="{ 'background-color': hueToColor(program.color) }"
-    >
-      <h2 class="text-xl">{{ program.name }}</h2>
-      <div class="flex flex-wrap text-xs text-gray-500 gap-1 justify-center">
-        <div v-for="(exercise, exerciseIndex) in program.exercises" :key="exercise._id" class="flex gap-1">
-          <span>{{ exercise.name }}</span>
-          <span>{{ exerciseIndex !== program.exercises.length - 1 ? '·' : '' }}</span>
+  <div class="col h-full relative">
+    <div class="col gap-3 flex-1 py-4 overflow-auto">
+      <NuxtLink v-for="program in programs" :key="program._id" :to="'/programs/' + program._id" class="list-item">
+        <h2>{{ program.name }}</h2>
+        <div class="flex flex-wrap text-xs text-gray-500 gap-1 justify-center">
+          <div v-for="(exercise, exerciseIndex) in program.exercises" :key="exercise._id" class="flex gap-1">
+            <span>{{ exercise.name }}</span>
+            <span>{{ exerciseIndex !== program.exercises.length - 1 ? '·' : '' }}</span>
+          </div>
         </div>
+      </NuxtLink>
+      <div class="mb-14" />
+    </div>
+    <NuxtLink class="absolute bottom-5 right-5 icon-16 text-indigo-500" @click="showModal=true"><PlusCircleIcon /></NuxtLink>
+    <Modal v-if="showModal" @close="showModal=false">
+      <h2>Création d'un programme</h2>
+      <label>Nom
+        <input v-model="newProgram.name" v-focus @keyup.enter="save">
+      </label>
+      <div class="flex gap-3 justify-center">
+        <button theme="primary" @click="save">Enregistrer</button>
+        <button theme="primary" @click="showModal=false">Fermer</button>
       </div>
-    </NuxtLink>
-    <NuxtLink to="/programs/create" class="absolute bottom-7 right-7 p-2 size-2xl border-2 rounded-full">＋</NuxtLink>
+    </Modal>
   </div>
 </template>
