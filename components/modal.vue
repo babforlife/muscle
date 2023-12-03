c6c2a6b0cce7a9bf87085787bb571d1f4646f876
Guillaume Malek <guillaume.malek@outlook.fr>
2023-12-03 14:39:57 +0100

    feat: update modal

--- components/modal.vue
+++ components/modal.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-defineProps<{childClass: string}>()
+defineProps<{childClass?: string}>()
 defineEmits(['close'])
 </script>
 
 <template>
-  <div class="fixed flex justify-center items-center inset-0 bg-cyan-100/25" @click.self="$emit('close')">
-    <div :class="childClass" @click.prevent.stop>
+  <div class="fixed flex justify-center items-center inset-0 bg-transparent/20" @click.self="$emit('close')">
+    <div class="bg-white p-4 rounded-lg shadow-xl col gap-6" :class="childClass" @click.prevent.stop>
       <slot />
     </div>
   </div>
