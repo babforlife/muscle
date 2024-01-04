398989d4412481da58be05bed0a0e00ebec61e7b
Guillaume Malek <guillaume.malek@outlook.fr>
2024-01-04 23:04:13 +0100

    add authentication custom component

---
+++ components/authentication.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-xl h-full col mx-7">
+    <div class="col items-center justify-center flex-1">
+      <img src="/logo.png" alt="MUSCLE">
+    </div>
+    <div class="flex-1">
+      <slot />
+    </div>
+    <div class="mt-auto py-4 text-lg text-center">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
